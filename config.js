c9ee38f80a3815f97edfa5723b6f8f4e39023425
Wreckingrival <97218064+Wreckingrival@users.noreply.github.com>
2026-02-08 20:37:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -80,7 +80,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dhfucw0je/video/upload/v1770562934/Tere_Hi_Hum_-_Prateek_Kuhad_Official_Lyric_Video_-_Prateek_Kuhad_vscu3m.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
